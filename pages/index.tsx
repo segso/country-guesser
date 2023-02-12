bbb5bfe19bc4771023b88d420bbef29d7c3b43cf
SEG <segdev.mx@gmail.com>
2023-02-12 01:12:31 -0600

    feat(page): add styles to the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
 import Layout from "@/components/layout";
 import Link from "next/link";
+import styles from "@/styles/home.module.css";
 
 export default function HomePage() {
   return (
     <Layout home>
-      <h1>
+      <h1 className={styles.title}>
         Country Guesser
       </h1>
-      <Link href="/play">
+      <Link href="/play" className={styles.play}>
         Play
       </Link>
     </Layout>
