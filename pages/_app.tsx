33293ccabf34fbe67802aa5fd510012940c4fa23
SEG <segdev.mx@gmail.com>
2024-09-14 02:19:59 -0600

    feat(page): add description meta and title tags to the _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,9 +7,12 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Web based quiz-like game of guessing the country's name by viewing its flag" />
+        <title>Country Guesser</title>
       </Head>
       <Component {...pageProps} />
     </>
-  );}
+  );
+}
 
 export default App;
